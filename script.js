6748d33f61f0898512ce0cd81f1ba57a2fced330
Murilo Ezequiel <muriloecastro@gmail.com>
2025-05-21 22:21:49 -0300

    reatualização geral, conexôes entre n.linguagens e atividades feitas, por mais que estas páginas não esjam prontas e atualização da página central para se adequar ao saral

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-document.getElementById('myButton').onclick = function(){menuOpener()}
-            
+
+          
 function menuOpener(){
         if (!document.querySelector(".vertical").classList.contains("ativo")) {
-                    document.querySelector(".vertical").classList.add("ativo");
+            document.querySelector(".vertical").classList.add("ativo");
         }
         else{
-                    document.querySelector(".vertical").classList.remove("ativo");
+            document.querySelector(".vertical").classList.remove("ativo");
         }
+    }   
+
+function atlogin(){
+    let usuario = prompt("usuario:");
+    if (usuario == "master"){
+        let senha = prompt("senha:");
+        if (senha == 'master'){
+            alert("ok");
+        }
+        else{
+            alert('esteje banido seu herege');
+        }
+        
     }
+    else{
+        alert("se retire herege");
+    }
+
+}
+async function atividadeBanco(){
+    const db = await open({
+        filename: "./banco.db",
+        driver: sqlite3.Database,
+    });
+    db.run('CREATE TABLE IF NOT EXISTS writers (id INTENGER PRIMARY KEY, usuario TEXT, senha TEXT)');
+   
+
+}
+
+
+document.getElementById('myButton').onclick = function(){menuOpener()}
+document.getElementById('add_atividade').onclick = function(){atlogin()}
